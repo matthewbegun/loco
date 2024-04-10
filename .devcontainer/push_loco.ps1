60644fff3387116bfecedf7355331ce56597ce7f
Matt Begun <matthewbegun@gmail.com>
2024-04-10 18:27:13 +1000

    clean and modern

--- .devcontainer/push_loco.ps1
+++
@@ -1,5 +0,0 @@
-$isodate = Get-Date -Format FileDateUniversal
-$targets = "os", "core", "data_science", "project", "latest", $isodate
-foreach ($target in $targets) {
-    docker push matthewbegun/loco:${target}
-}
